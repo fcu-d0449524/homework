680a64435d821fce3fd73f7494f7831a95d6c669
Lin Shinhong <Lin Shinhong@DESKTOP-UEFS22M>
2016-10-05 22:28:46 +0800

    原歌詞

---
+++ src/fcu/iecs/oop/homework.java
@@ -0,0 +1,16 @@
+package fcu.iecs.oop;
+
+public class homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		
+		
+		
+		
+
+	}
+
+}
